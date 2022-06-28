273e521a0d63231977962b732b06c3c0503cf748
Alex2 <aleksander1802@gmail.com>
2022-06-28 13:21:27 +0300

    init: section agent, section price

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,28 @@
 
+// function count (string) {  
+//     var count = {};
+//     string.split('').forEach(function(s) {
+//        count[s] ? count[s]++ : count[s] = 1;
+//     });
+//     return count;
+//   }
+
+
+
+
+// function foobar(count) {
+//     let array = (new Array(count)).fill(1).map((_, x) => x + 1);
+//     let arr = array.map( x => x % 3 === 0 && x % 5 === 0 
+        // ? 'FOOBAR' : x % 3 === 0 
+        // ? 'Foo' : x % 5 === 0 
+        // ? 'Bar' : x)
+//     return arr
+// }
+
+// console.log(foobar(100));
+
+
+
 
 
 
